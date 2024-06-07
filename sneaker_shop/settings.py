cfb5063edcc9401d233e375286465f9bcbdfbba4
aswinmano <163403395+aswinmano@users.noreply.github.com>
2024-06-07 12:26:02 +0530

    Update settings.py

--- sneaker_shop/settings.py
+++ sneaker_shop/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'sneaker_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
